db5e1f41782a9cd428b0b7866b20eb12062f816f
Chingis Choybsonov <avatarius93@gmail.com>
2024-10-19 02:32:00 +0300

    что то вроде первой рабочей версии

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 const cabinetContainer = document.querySelector(".cabinet__container");
-
+const basket = document.querySelector(".basket");
+const basketButton = document.querySelector('.basket__button');
+let basketSize = 0;
 const productList = document.querySelectorAll(".cabinet__item");
 
-
+let currentDroppable = null;
 
 function handleDnd(e) {
   const { currentTarget, pageX, pageY, clientX, clientY } = e;
@@ -11,22 +13,53 @@ function handleDnd(e) {
   const shiftX = clientX - offsetX;
   const shiftY = clientY - offsetY;
   cabinetContainer.append(currentTarget);
+
   function moveAt(x, y) {
     const resultX = x - cabinetContainer.getBoundingClientRect().left;
     const resultY = y - cabinetContainer.getBoundingClientRect().top;
-    currentTarget.style.insetInlineStart = `${resultX - shiftX}px`
-    currentTarget.style.insetBlockStart = `${resultY - shiftY}px`
+    currentTarget.style.insetInlineStart = `${resultX - shiftX}px`;
+    currentTarget.style.insetBlockStart = `${resultY - shiftY}px`;
   }
-  moveAt(pageX, pageY);window
+
+  function reset() {
+    currentTarget.style = "";
+  }
+
+  moveAt(pageX, pageY);
   function handleMouseMove(e) {
-    const { pageX, pageY } = e;
+    const { pageX, pageY, clientX, clientY } = e;
     moveAt(pageX, pageY);
+    currentTarget.hidden = true;
+    const elemBelow = document.elementFromPoint(clientX, clientY);
+    currentTarget.hidden = false;
+    if (!elemBelow) {
+      return;
+    }
+    const droppableBelow = elemBelow.closest(".basket");
+    if (currentDroppable !== droppableBelow) {
+      currentDroppable = droppableBelow;
+    }
   }
 
   document.addEventListener("mousemove", handleMouseMove);
   currentTarget.onmouseup = () => {
     document.removeEventListener("mousemove", handleMouseMove);
     currentTarget.onmouseup = null;
+    console.log(currentDroppable);
+    if (!currentDroppable) {
+      reset();
+      currentTarget.setAttribute('data-in-basket', false);
+    } else {
+      currentTarget.setAttribute('data-in-basket', true);
+    }
+    const basket = document.querySelectorAll('.cabinet__item[data-in-basket=true]');;
+
+    if (basket.length >= 3) {
+      basketButton.classList.remove('basket__button_hidden');
+    } else {
+      basketButton.classList.add('basket__button_hidden');
+    }
+
   };
 }
 
