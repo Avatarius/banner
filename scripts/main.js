4760c0805e3c89ea61085d072b79a12e9aa83c46
Chingis Choybsonov <avatarius93@gmail.com>
2024-10-21 01:40:04 +0300

    начало работы над перетаскиванием из корзины

--- scripts/main.js
+++ scripts/main.js
@@ -5,14 +5,19 @@ const basketButton = document.querySelector(".basket__button");
 let basketSize = 0;
 const productList = document.querySelectorAll(".product");
 let currentDroppable = null;
+let targetCopy;
 
 let inlineOffset = 0;
 let previousWidth = 0;
 let overallWidth = 0;
 
+function setListeners(item) {
+  item.addEventListener("mousedown", handleDnd);
+  item.ondragstart = () => false;
+}
+
 function handleDnd(e) {
   const { currentTarget, pageX, pageY, clientX, clientY } = e;
-  let targetCopy;
   const offsetX = currentTarget.getBoundingClientRect().left;
   const offsetY = currentTarget.getBoundingClientRect().top;
   const shiftX = clientX - offsetX;
@@ -74,15 +79,15 @@ function handleDnd(e) {
     currentTarget.onmouseup = null;
     if (!currentDroppable) {
       reset(currentTarget);
-      // currentTarget.setAttribute('data-in-basket', false);
+      currentTarget.classList.add(`product__${targetCopy.dataset.name}`);
+      currentTarget.classList.remove('product_in-basket');
     } else {
-      // currentTarget.setAttribute('data-in-basket', true);
       targetCopy = currentTarget.cloneNode(true);
       currentTarget.classList.add("product_hidden");
       targetCopy.classList.remove(`product__${targetCopy.dataset.name}`);
       targetCopy.classList.add("product_in-basket");
+      setListeners(targetCopy);
       basketContainer.append(targetCopy);
-
       moveCopy();
     }
 
@@ -95,6 +100,5 @@ function handleDnd(e) {
 }
 
 productList.forEach((item) => {
-  item.addEventListener("mousedown", handleDnd);
-  item.ondragstart = () => false;
+  setListeners(item);
 });
